1788709b07e8a29cd6fbfa56ba610d4f16bea561
Yosuke Nakano <ynakano@moff.mobi>
2019-07-02 12:42:30 +0900

    add View

---
+++ ViewPattern/Assets/Scripts/View.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ViewPatterns {
+    public class View : MonoBehaviour {
+        [SerializeField] InputField nameInputField = default;
+        [SerializeField] InputField ageInputField = default;
+        [SerializeField] InputField heightInputField = default;
+        [SerializeField] InputField weightInputField = default;
+        [SerializeField] Text bimText = default;
+        InputField NameInputField { get { return nameInputField; } }
+        InputField AgeInputField { get { return ageInputField; } }
+        InputField HeightInputField { get { return heightInputField; } }
+        InputField WeightInputField { get { return weightInputField; } }
+        Text BMIText { get { return bimText; } }
+    }
+}

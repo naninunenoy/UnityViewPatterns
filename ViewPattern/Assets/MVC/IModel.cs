1d4b6ba4ab6e7392fa134c669da173e54b0bbb1e
Yosuke Nakano <ynakano@moff.mobi>
2019-07-02 16:05:22 +0900

    move files

--- ViewPattern/Assets/MVC/IModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace ViewPatterns.MVC {
-    public interface IModel<TEntity, TResult> {
-        bool TryApply(TEntity data, out TResult result);
-    }
-}
---
+++ ViewPattern/Assets/Scripts/BMIModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ViewPatterns {
+    public class BMIModel : IModel<PersonEntity, float> {
+        public bool TryApply(PersonEntity data, out float result) {
+            result = 0.0F;
+            if (data == null) {
+                return false;
+            }
+            if (Mathf.Approximately(0.0F, data.Height)) {
+                return false;
+            }
+            // BIM = 体重[kg] / 身長[m]^2
+            var merter = data.Height / 100.0F;
+            result = data.Weight / (merter * merter);
+            return true;
+        }
+    }
+}

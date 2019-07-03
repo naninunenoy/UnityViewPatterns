299d10f7e7b2bb0ed1ce65a02003968ae1d39198
Yosuke Nakano <ynakano@moff.mobi>
2019-07-03 15:57:20 +0900

    IView

--- ViewPattern/Assets/Scripts/BMIView.cs
+++ ViewPattern/Assets/Scripts/BMIView.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 
 namespace ViewPatterns {
-    public class BMIView : MonoBehaviour {
+    public class BMIView : MonoBehaviour, IView {
         [SerializeField] InputField nameInputField = default;
         [SerializeField] InputField ageInputField = default;
         [SerializeField] InputField heightInputField = default;

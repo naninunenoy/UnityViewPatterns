19c8964f75892e8c74bafb4603a61a96ecfafc35
nakano yosuke <naninuneno0411@gmail.com>
2019-07-27 16:57:20 +0900

    ダミーの認証

---
+++ ViewPattern/Assets/BMIApp/Scripts/Login/Authentification/DummyAuthController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using UnityEngine;
+
+namespace BMIApp.Login {
+    public class DummyAuthController : IAuthController {
+        struct User {
+            public string id;
+            public string password;
+            public string token;
+        }
+
+        readonly User dummyUser1 = new User { id = "tanaka", password = "tanaka", token = "tanaka" };
+        readonly User dummyUser2 = new User { id = "testuser", password = "password123", token = "muttya_nagai_mojiretsu" };
+        readonly Dictionary<string, User> authUsers;
+
+        public DummyAuthController() {
+            var users = new User[] { dummyUser1, dummyUser2 };
+            authUsers = users.ToDictionary(x => x.id);
+        }
+
+        public async Task<string> TryGetTokenAsync(string id, string password) {
+            if (!authUsers.TryGetValue("id", out User user) || user.password != password)  {
+                return string.Empty;
+            }
+            return await Task.FromResult(user.token);
+        }
+    }
+}
---
+++ ViewPattern/Assets/BMIApp/Scripts/Login/Authentification/IAuthController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace BMIApp.Login {
+    public interface IAuthController : CleanArchitecture.IController {
+        Task<string> TryGetTokenAsync(string id, string password);
+    }
+}

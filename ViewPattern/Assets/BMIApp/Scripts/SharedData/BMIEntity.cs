f2b78c3576f95e66ed8aa93173e9e849ed459c14
nakano yosuke <naninuneno0411@gmail.com>
2019-08-01 01:29:11 +0900

    形式修正

--- ViewPattern/Assets/BMIApp/Scripts/SharedData/BMIEntity.cs
+++ ViewPattern/Assets/BMIApp/Scripts/SharedData/BMIEntity.cs
@@ -6,30 +6,32 @@ using UnityEngine;
 namespace BMIApp {
     [Serializable]
     public class BMIEntity : IBMIEntity {
+        const string dateTimeFormat = "yyyy-MM-dd+HH:mm:ss";
+
         [SerializeField] string name;
         [SerializeField] float height;
         [SerializeField] float weight;
         [SerializeField] int age;
-        [SerializeField] Gender gender;
+        [SerializeField] string gender;
         [SerializeField] float bmi;
-        [SerializeField] DateTime createdAt;
+        [SerializeField] string createdAt;
 
         public BMIEntity() {
             name = string.Empty;
             height = 0.0F;
             weight = 0.0F;
             age = 0;
-            gender = Gender.None;
+            gender = Gender.None.ToString();
             bmi = 0.0F;
-            createdAt = DateTime.MinValue;
+            createdAt = DateTime.MinValue.ToString(dateTimeFormat);
         }
 
         public string Name { set { name = value; } get => name; }
         public float Height { set { height = value; } get => height; }
         public float Weight { set { weight = value; } get => weight; }
         public int Age { set { age = value; } get => age; }
-        public Gender Gender { set { gender = value; } get => gender; }
+        public Gender Gender { set { gender = value.ToString(); } get => (Gender)Enum.Parse(typeof(Gender), gender); }
         public float BMI { set { bmi = value; } get => bmi; }
-        public DateTime CreatedAt { set { createdAt = value; } get => createdAt; }
+        public DateTime CreatedAt { set { createdAt = value.ToString(dateTimeFormat); } get => DateTime.Parse(createdAt); }
     }
 }

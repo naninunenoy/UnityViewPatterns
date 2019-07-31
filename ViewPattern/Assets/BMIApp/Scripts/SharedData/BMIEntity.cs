6d018ee3d1a08010ff6bd9d8a7ca9826a76e29d7
nakano yosuke <naninuneno0411@gmail.com>
2019-08-01 01:18:25 +0900

    entity追加

---
+++ ViewPattern/Assets/BMIApp/Scripts/SharedData/BMIEntity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BMIApp {
+    [Serializable]
+    public class BMIEntity : IBMIEntity {
+        [SerializeField] string name;
+        [SerializeField] float height;
+        [SerializeField] float weight;
+        [SerializeField] int age;
+        [SerializeField] Gender gender;
+        [SerializeField] float bmi;
+        [SerializeField] DateTime createdAt;
+
+        public BMIEntity() {
+            name = string.Empty;
+            height = 0.0F;
+            weight = 0.0F;
+            age = 0;
+            gender = Gender.None;
+            bmi = 0.0F;
+            createdAt = DateTime.MinValue;
+        }
+
+        public string Name { set { name = value; } get => name; }
+        public float Height { set { height = value; } get => height; }
+        public float Weight { set { weight = value; } get => weight; }
+        public int Age { set { age = value; } get => age; }
+        public Gender Gender { set { gender = value; } get => gender; }
+        public float BMI { set { bmi = value; } get => bmi; }
+        public DateTime CreatedAt { set { createdAt = value; } get => createdAt; }
+    }
+}
---
+++ ViewPattern/Assets/BMIApp/Scripts/SharedData/IBMIEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BMIApp {
+    public enum Gender { None = 0, Male, Female }
+    public interface IBMIEntity : CleanArchitecture.IEntity {
+        string Name { set; get; }
+        float Height { set; get; }
+        float Weight { set; get; }
+        int Age { set; get; }
+        Gender Gender { set; get; }
+        float BMI { set; get; }
+        DateTime CreatedAt { set; get; }
+    }
+}

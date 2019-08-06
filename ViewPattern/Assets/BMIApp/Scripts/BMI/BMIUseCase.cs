76dfe31980564db65116914bea1a7c2401d5a115
nakano yosuke <naninuneno0411@gmail.com>
2019-08-07 00:53:17 +0900

    BMIのusecase実装

--- ViewPattern/Assets/BMIApp/Scripts/BMI/BMIUseCase.cs
+++ ViewPattern/Assets/BMIApp/Scripts/BMI/BMIUseCase.cs
@@ -1,10 +1,112 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
 
 namespace BMIApp.BMI {
     public class BMIUseCase : CleanArchitecture.IUseCase {
+        readonly IBMIPresenter bmiPresenter;
+        readonly IBMIHistoryRepository<IBMIEntity> bmiRepository;
+        readonly Component disposableComponent;
+
+        public BMIUseCase(IBMIPresenter bmiPresenter,
+                          IBMIHistoryRepository<IBMIEntity> bmiRepository,
+                          Component disposableComponent) {
+            this.bmiPresenter = bmiPresenter;
+            this.bmiRepository = bmiRepository;
+            this.disposableComponent = disposableComponent;
+        }
+
         public void Begin() {
+            var entity = new BMIEntity();
+            bmiPresenter.Begin();
+            bmiPresenter
+                .NameInput
+                .Subscribe(x => {
+                    entity.Name = x;
+                })
+                .AddTo(disposableComponent);
+            bmiPresenter
+                .HeightInput
+                .Subscribe(x => {
+                    if (float.TryParse(x, out var val)) {
+                        entity.Height = val;
+                    }
+                })
+                .AddTo(disposableComponent);
+            bmiPresenter
+                .WeightInput
+                .Subscribe(x => {
+                    if (float.TryParse(x, out var val)) {
+                        entity.Weight = val;
+                        entity.BMI = UpdateBMI(entity);
+                    }
+                })
+                .AddTo(disposableComponent);
+            bmiPresenter
+                .AgeInput
+                .Subscribe(x => {
+                    if (int.TryParse(x, out var val)) {
+                        entity.Age = val;
+                        entity.BMI = UpdateBMI(entity);
+                    }
+                })
+                .AddTo(disposableComponent);
+            Observable
+                .CombineLatest(bmiPresenter.GenderMaleSelect, bmiPresenter.GenderFemaleSelect)
+                .Subscribe(x => {
+                    if (x[0]) {
+                        entity.Gender = Gender.Male;
+                    } else if (x[1]) {
+                        entity.Gender = Gender.Female;
+                    } else {
+                        entity.Gender = Gender.None;
+                    }
+                })
+                .AddTo(disposableComponent);
+            bmiPresenter
+                .SaveButtonClickObservable
+                .Subscribe(async _ => {
+                    entity.CreatedAt = DateTime.Now;
+                    await bmiRepository.SaveAsync(entity);
+                })
+                .AddTo(disposableComponent);
+        }
+
+        float UpdateBMI(BMIEntity entity) {
+            if (!TryCalcBMI(entity.Height, entity.Weight, out float bmi)) {
+                return 0.0F;
+            }
+            var msg = GetBMIEvaluation(bmi);
+            bmiPresenter.SetBMIResult($"{bmi:F1}({msg})");
+            return bmi;
+        }
+
+        bool TryCalcBMI(float heightCm, float weightKg, out float bmi) {
+            if (heightCm <= 0.0F || weightKg <= 0.0F) {
+                bmi = 0.0F;
+                return false;
+            }
+            var h = heightCm / 100.0F; // m -> cm
+            bmi = weightKg / (h * h);
+            return true;
+        }
+
+        string GetBMIEvaluation(float bmi) {
+            if (bmi < 16.0F) {
+                return "やせすぎ";
+            } else if (bmi < 17.0F) {
+                return "やせ";
+            } else if (bmi < 18.5F) {
+                return "やせ気味";
+            } else if (bmi < 25.0F) {
+                return "普通";
+            } else if (bmi < 30.0F) {
+                return "肥満気味";
+            } else {
+                return "肥満";
+            }
         }
     }
 }

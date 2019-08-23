2b93ce5f839f26c61195a739e9f489a084d589cc
Yosuke Nakano <ynakano@moff.mobi>
2019-08-23 18:54:40 +0900

    HistoryUseCase作成

---
+++ ViewPattern/Assets/BMIApp/Scripts/BMI/HistoryUseCase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BMIApp.BMI {
+    public class HistoryUseCase<TEntity> : CleanArchitecture.IUseCase where TEntity : IBMIEntity {
+        readonly IHistoryListPresenter historyPresenter;
+        readonly IBMIHistoryRepository<TEntity> historyRepository;
+        readonly Component disposableComponent;
+
+        public HistoryUseCase(IHistoryListPresenter historyPresenter, 
+                              IBMIHistoryRepository<TEntity> historyRepository,
+                              Component disposableComponent) {
+            this.historyPresenter = historyPresenter;
+            this.historyRepository = historyRepository;
+            this.disposableComponent = disposableComponent;
+        }
+
+        public void Begin() {
+
+        }
+    }
+}

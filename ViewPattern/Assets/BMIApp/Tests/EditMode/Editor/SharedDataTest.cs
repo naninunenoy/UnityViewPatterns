d40cdfac610a1b0a99ec987e96ca378fe6da7d04
Yosuke Nakano <ynakano@moff.mobi>
2019-09-03 19:31:16 +0900

    SharedDataのテスト

---
+++ ViewPattern/Assets/BMIApp/Tests/EditMode/Editor/SharedDataTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace BMIApp.Tests {
+    public class SharedDataTest {
+        [Test]
+        public void SharedScriptableObjectTest() {
+            var obj = ScriptableObject.CreateInstance<SharedScriptableObject>();
+            Assert.NotNull(obj);
+            Assert.IsEmpty(obj.CurrentUserId);
+            Assert.IsEmpty(obj.CurrentUserToken);
+            obj.CurrentUserId = "test_id";
+            obj.CurrentUserToken = "test_token";
+            Assert.That(obj.CurrentUserId, Is.EqualTo("test_id"));
+            Assert.That(obj.CurrentUserToken, Is.EqualTo("test_token"));
+        }
+
+        [Test]
+        public void UserAccountRepositoryTest() {
+            var obj = ScriptableObject.CreateInstance<SharedScriptableObject>();
+            var rep = new UserAccountRepository(obj);
+            Assert.NotNull(rep);
+            Assert.IsEmpty(rep.CurrentUserId);
+            Assert.IsEmpty(rep.CurrentUserToken);
+            rep.CurrentUserId = "test_id";
+            rep.CurrentUserToken = "test_token";
+            Assert.That(rep.CurrentUserId, Is.EqualTo("test_id"));
+            Assert.That(rep.CurrentUserToken, Is.EqualTo("test_token"));
+        }
+    }
+}

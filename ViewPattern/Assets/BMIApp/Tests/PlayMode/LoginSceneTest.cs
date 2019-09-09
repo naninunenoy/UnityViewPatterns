c8f2497bec96737efa57d9b8aeeb63d696cad605
nakano yosuke <naninuneno0411@gmail.com>
2019-09-10 00:36:16 +0900

    scene load test

---
+++ ViewPattern/Assets/BMIApp/Tests/PlayMode/LoginSceneTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+using BMIApp.Login;
+
+namespace BMIApp.Tests.PlayMode {
+    public class LoginSceneTest {
+        LoginMain main = default;
+
+        [UnitySetUp]
+        public IEnumerator SetUpTest() {
+            var load = SceneManager.LoadSceneAsync("Login");
+            while (!load.isDone) {
+                yield return null;
+            }
+        }
+
+        [UnityTearDown]
+        public IEnumerator TearDownTest() {
+            main = default;
+            yield return null;
+        }
+
+        [UnityTest]
+        public IEnumerator FindMainTest() {
+            main = GameObject.Find("LoginMain").GetComponent<LoginMain>();
+            Assert.IsNotNull(main);
+            yield return null;
+        }
+    }
+}

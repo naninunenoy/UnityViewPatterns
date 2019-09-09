30279e5d4c58c5c6100a0cdddcc896469b053cd5
nakano yosuke <naninuneno0411@gmail.com>
2019-09-10 00:13:42 +0900

    テストのサンプル

---
+++ ViewPattern/Assets/BMIApp/Tests/PlayMode/TestSample.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests {
+    public class TestSample {
+        System.Diagnostics.Stopwatch sw;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp() {
+            sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+            Debug.Log($"OneTimeSetUpTest t={sw.ElapsedMilliseconds}");
+        }
+
+        [SetUp]
+        public void SetUpTest() {
+            Debug.Log($"SetUpTest t={sw.ElapsedMilliseconds}");
+        }
+
+        [TearDown]
+        public void TearDwonTest() {
+            Debug.Log($"TearDwonTest t={sw.ElapsedMilliseconds}");
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDownTest() {
+            Debug.Log($"OneTimeTearDownTest t={sw.ElapsedMilliseconds}");
+            sw.Stop();
+        }
+
+        [Test]
+        public void DoTest2() {
+            Debug.Log($"DoTest2 t={sw.ElapsedMilliseconds}");
+        }
+
+        [Test]
+        public void DoTest1() {
+            Debug.Log($"DoTest1 t={sw.ElapsedMilliseconds}");
+        }
+
+        [Test]
+        [Order(-1)]
+        public void DoTest99() {
+            Debug.Log($"DoTest99 t={sw.ElapsedMilliseconds}");
+        }
+
+        [Test]
+        [Order(1)]
+        public void DoTest0() {
+            Debug.Log($"DoTest0 t={sw.ElapsedMilliseconds}");
+        }
+    }
+}
---
+++ ViewPattern/Assets/BMIApp/Tests/PlayMode/UnityTestSample.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests {
+    public class UnityTestSample {
+        System.Diagnostics.Stopwatch sw;
+
+        [OneTimeSetUp]
+        //[OneTimeUnitySetUp]は無い
+        public void OneTimeSetUp() {
+            sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+            Debug.Log($"OneTimeSetUpTest t={sw.ElapsedMilliseconds}");
+        }
+        
+        [UnitySetUp]
+        public IEnumerator SetUpTest() {
+            Debug.Log($"SetUpUnityTest t={sw.ElapsedMilliseconds}");
+            yield return null;
+        }
+
+        [UnityTearDown]
+        public IEnumerator TearDownTest() {
+            Debug.Log($"TearDownUnityTest t={sw.ElapsedMilliseconds}");
+            yield return null;
+        }
+
+        [OneTimeTearDown]
+        //[OneTimeUnityTearDown]は無い
+        public void OneTimeTearDownTest() {
+            Debug.Log($"OneTimeTearDownTest t={sw.ElapsedMilliseconds}");
+            sw.Stop();
+        }
+
+        [UnityTest]
+        public IEnumerator DoUnityTest2() {
+            Debug.Log($"DoUnityTest2 t={sw.ElapsedMilliseconds}");
+            yield return null;
+        }
+
+        [UnityTest]
+        public IEnumerator DoUnityTest1() {
+            Debug.Log($"DoUnityTest1 t={sw.ElapsedMilliseconds}");
+            yield return null;
+        }
+
+        [UnityTest]
+        [Order(-1)]
+        public IEnumerator DoUnityTest99() {
+            Debug.Log($"DoUnityTest99 t={sw.ElapsedMilliseconds}");
+            yield return null;
+        }
+
+        [UnityTest]
+        [Order(1)]
+        public IEnumerator DoUnityTest0() {
+            Debug.Log($"DoUnityTest99 t={sw.ElapsedMilliseconds}");
+            yield return null;
+        }
+    }
+}
